5906efb4a9d08f5fd467f3366e27c2b961a9f1ca
Nonkik589 <michaelbullock0106@gmail.com>
2023-06-22 15:57:00 -0400

    Changes api host and includes minor fixes to html

--- Database Creation and App/app.py
+++ Database Creation and App/app.py
@@ -1,14 +1,12 @@
-import numpy as np
-import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, MetaData
-from flask import Flask, render_template, jsonify
-from flask_cors import CORS, cross_origin
+from sqlalchemy import create_engine
+from flask import Flask, jsonify
+
 
 # Setting up the DB
-engine = create_engine(r"sqlite:///Data/ILINet.db")
+engine = create_engine(r"sqlite:////home/Nonkik589/mysite/Data/ILINet.db")
 
 Base = automap_base()
 # Reflect the tables
@@ -19,26 +17,21 @@ ILINet = Base.classes.ilinet
 
 # Flask setup
 app = Flask(__name__)
-cors = CORS(app)
 
 ################
 # Flask routes
 ################
 
+
 @app.route("/")
 def home():
-    return render_template('index.html')
-
-@app.route("/api")
-@cross_origin()
-def api():
     "Presents the JSON file"
-    
+
     # Create the session link
     session = Session(engine)
 
     # Querying the data for jsonifying
-    results = session.query(ILINet.REGION, ILINet.YEAR, ILINet.WEEK, 
+    results = session.query(ILINet.REGION, ILINet.YEAR, ILINet.WEEK,
                             ILINet.PERCENTUNWEIGHTEDILI, ILINet.ILITOTAL,
                             ILINet.NUMOFPROVIDERS, ILINet.TOTALPATIENTS).all()
 
@@ -61,4 +54,4 @@ def api():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=False)
