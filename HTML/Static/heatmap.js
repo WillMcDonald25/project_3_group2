edf53ba46429929218f5d94dad2372f28a53457f
Kennatha <125104416+Kennatha@users.noreply.github.com>
2023-06-22 21:16:54 -0500

    Heatmap updates

--- HTML/Static/heatmap.js
+++ HTML/Static/heatmap.js
@@ -1,30 +1,35 @@
-// var myMap = L.map("mapKW", {
-//     center: [37.7749, -122.4194],
-//     zoom: 4
-//   });
+var myMap = L.map("mapKW", {
+  center: [37.09, -120.71],
+  zoom: 13
+  });
   
-//   // Adding the tile layer
-//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   }).addTo(myMap);
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
   
   var url = "http://nonkik589.pythonanywhere.com/";
   
   var stateCoord="https://gist.githubusercontent.com/meiqimichelle/7727723/raw/0109432d22f28fd1a669a3fd113e41c4193dbb5d/USstates_avg_latLong";
 
   
+var states=[];
 
   d3.json(stateCoord).then(function(data) {
-    console.log(data);
+    console.log(data,"State Coordinates");
 
-    var states=[];
+    
     for (var k = 0; k < data.length; k++){
         var stateData = data[k];
         if (stateData){
             states.push({"Name":stateData.state,"Coordinates":[stateData.latitude,stateData.longitude]});
-        };
+          
     }
-  });
+  };
+  console.log(states,"States");
+});
+
+
   d3.json(url).then(function(response) {
   
     console.log(response);
@@ -33,13 +38,16 @@
   
     for (var i = 0; i < response.length; i++) {
       var location = response[i].Region;
-      for (var k = 0; k< states.length; k++){
+      
+      for (var j = 0; j< states.length; j++){      
+        if (location[0] === states[j].Name) {          
+            heatArray.push(states[j].Coordinates);
+        }
+    
+        };};
   
-        if (location == states[k].Name) {
-            heatArray.push([states.Coordinates[0], states.Coordinates[1]]);
-        };
-        }}
-  
+    console.log(heatArray,"Heat Array"); 
+      
     var heat = L.heatLayer(heatArray, {
       radius: 20,
       blur: 35
