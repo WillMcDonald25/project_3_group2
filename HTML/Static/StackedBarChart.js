1fd9b65e8779f7c0249caab6fc005c42fa08b76a
Nonkik589 <michaelbullock0106@gmail.com>
2023-06-26 21:34:09 -0400

    Modifies the Readme

--- HTML/Static/StackedBarChart.js
+++ HTML/Static/StackedBarChart.js
@@ -1,49 +1,78 @@
 getData();
+
+   newLabels = []; patientData = []; providerData = [];
+
    async function getData() {
-      const response = await fetch('http://nonkik589.pythonanywhere.com/');
+      const newurl = 'http://nonkik589.pythonanywhere.com/';
+      const response = await fetch(newurl);
       const data = await response.json();
       console.log(data);
       const length = data.length;
       console.log(length);
-      console.log(data.Year)
-      data2013 = [];
-      data2014 = [];
-      data2015 = [];
-      data2016 = [];
+      const year = data.map( (x) => x.Year[0])
+      const patient = data.map( (x) => x.Number_of_Patients)
+      const provider = data.map( (x) => x.Number_of_Providers[0])
+      console.log("Year", year,"patient", patient,"provider", provider);
+
+        
+      var patientData = patient;
+      var providerData = provider;
+
       for (j = 0; j < length; j++) {
-         if (data.Year == 2013) {
-            data2013.push(data[j].Number_of_Patients)
+         var patientdata2013 = 0;
+         var patientdata2014 = 0;
+         var patientdata2015 = 0;
+         var patientdata2016 = 0;
+         console.log("YEAR(J)", year[j])
+         if (year[j] === 2013) {
+            patientdata2013 += parseInt(patientData[j]);
+            console.log(patientData[j],  "data2013");
          }
-         else if (data.Year == 2014) {
-            data2014.push(data[j].Number_of_Patients)
+         else if (year[j] === 2014) {
+            patientdata2014 += patientData[j];
+            console.log(patientData[j],  "data2014");
          }
-         else if (data.Year == 2015) {
-            data2015.push(data[j].Number_of_Patients)
+         else if (year[j] === 2015) {
+            patientdata2015 += (patientData[j]);
+            console.log(patientData[j],  "data2015");
          }
-         else if (data.Year == 2016) {
-            data2016.push(data[j].Number_of_Patients)
+         else {
+            patientdata2016 += (patientData[j]);
+            console.log(patientData[j],  "data2016");
          }
       };
 
+      
+      console.log("2013", patientdata2013);
+      labels = [];
       values = [];
       for (i = 0; i < length; i++) {
+         labels.push(data[i].Year);
          values.push(data[i].Number_of_Patients);
       }
       new Chart(document.getElementById("myChart"), {
          type: 'bar',
          data: {
-            labels: [data2013.Year, data2014.Year, data2015.Year, data2016.Year],
+            labels: ["2013"],
             datasets: [
                {
                label: "Number of Infected Patients",
-               data: [data2013.Number_of_Patients, data2014.Number_of_Patients, data2015.Number_of_Patients, data2016.Number_of_Patients],
+               data: patientdata2013,
                backgroundColor: "red",
                width: 800,
                height: 600
-               }
+               },
+               {
+                  label: "Number of Providers",
+                  data: providerData,
+                  backgroundColor: "blue",
+                  width: 800,
+                  height: 600
+                  }
             ]
          },
          options: {
+            mode: indexedDB,
             legend: { display: true },
             title: {
                display: true,
@@ -52,6 +81,7 @@ getData();
          },
          scales: {
             xAxes: [{
+               barWidth: 1,
               stacked: true,
               gridLines: {
                 display: false,
